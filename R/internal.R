7ad539c204216f7ba00dda0369d9aaa184f86dac
Ryan Patrick Kyle <ryan@plot.ly>
2019-12-31 02:28:34 -0500

    add map

--- R/internal.R
+++ R/internal.R
@@ -28,6 +28,12 @@ version = "0.3.1", src = list(href = NULL,
 file = "deps"), meta = NULL,
 script = 'dash_daq.min.js',
 stylesheet = NULL, head = NULL, attachment = NULL, package = "dashDaq",
-all_files = FALSE), class = "html_dependency"))
+all_files = FALSE), class = "html_dependency"),
+`dash_daq` = structure(list(name = "dash_daq",
+version = "0.3.1", src = list(href = NULL,
+file = "deps"), meta = NULL,
+script = 'dash_daq.min.js.map',
+stylesheet = NULL, head = NULL, attachment = NULL, package = "dashDaq",
+all_files = FALSE, dynamic = TRUE), class = "html_dependency"))
 return(deps_metadata)
 }
